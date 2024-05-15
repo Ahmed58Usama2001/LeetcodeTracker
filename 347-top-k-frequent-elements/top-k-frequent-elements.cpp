29efbc1a4df0a4df0041e9913352d694a641a027
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2024-05-15 22:43:07 +0300

    Time: 15 ms (32.33%) | Memory: 17.3 MB (23.18%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -11,13 +11,17 @@ public:
                hash[nums[i]]++; 
         }
         
-        vector<pair<int, int>> mapVector(hash.begin(), hash.end());
-        sort(mapVector.begin(), mapVector.end(), compareBySecond);
+      priority_queue<pair<int,int>>pq;
+      for(auto it=hash.begin();it!=hash.end();it++)
+      {
+        pq.push(make_pair(it->second,it->first));
+      }
 
-        for(int i=0;i<k;i++)
-        {
-          result.push_back(mapVector[i].first);
-        }
+      for(int i=0;i<k;i++)
+      {
+        result.push_back(pq.top().second);
+        pq.pop();
+      }
 
     return result;
   
