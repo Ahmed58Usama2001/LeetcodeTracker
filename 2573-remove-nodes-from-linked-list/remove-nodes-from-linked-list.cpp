864f45818b614063cf5cc6d8dab5f97e8a007a87
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2024-09-07 21:43:45 +0300

    Time: 297 ms (40.69%) | Memory: 177.3 MB (16.40%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        //use stack to remove the elements if cur>stk.top
+        //use another stack to reverse
+        //make a linked lis from the elements in thereversed stack
+        vector<int>stk;
+
+        ListNode* cur=head;
+
+        while(cur)
+        {
+            while(!stk.empty() && cur->val>stk.back())
+                stk.pop_back();
+            
+            stk.push_back(cur->val);
+            cur=cur->next;
+        }
+
+
+
+        ListNode* Result=new ListNode(0);
+         cur=Result;
+        
+
+        for(int i=0;i<stk.size();i++)
+        {
+            ListNode* node=new ListNode(stk[i]);
+            cur->next=node;
+            cur=cur->next;
+        }
+
+        return Result->next;
+    }
+};
