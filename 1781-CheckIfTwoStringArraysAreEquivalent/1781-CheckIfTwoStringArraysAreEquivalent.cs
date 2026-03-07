6b3f3558fded828abe1927892b5d064677356b49
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:17:24 +0200

    Create 1781-CheckIfTwoStringArraysAreEquivalent

---
+++ 1781-CheckIfTwoStringArraysAreEquivalent/1781-CheckIfTwoStringArraysAreEquivalent.cs
@@ -0,0 +1,7 @@
+// Last updated: 3/7/2026, 4:17:23 PM
+public class Solution {
+    public bool ArrayStringsAreEqual(string[] word1, string[] word2) {
+        return string.Concat(word1) == string.Concat(word2);
+        
+    }
+}
