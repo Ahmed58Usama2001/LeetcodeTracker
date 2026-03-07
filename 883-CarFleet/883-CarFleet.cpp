e23837b1a360aed0d70dc27611f7cace28ec7ed1
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:15:42 +0200

    Create 883-CarFleet

---
+++ 883-CarFleet/883-CarFleet.cpp
@@ -0,0 +1,24 @@
+// Last updated: 3/7/2026, 4:15:42 PM
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        map<int,double>m;
+
+        for(int i=0;i<position.size();i++)
+            m[-position[i]]=(double)(target-position[i])/speed[i];
+
+        int res=0;
+        double cur=0;
+
+        for(auto it:m)
+        {
+            if(it.second>cur)
+            {
+                cur=it.second;
+                res++;
+            }
+        }
+
+        return res;
+    }
+};
