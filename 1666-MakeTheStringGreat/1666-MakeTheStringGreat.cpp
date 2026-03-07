dcdf180f18f65504f84acd0844934095eb765cfa
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:12:01 +0200

    Create 1666-MakeTheStringGreat

---
+++ 1666-MakeTheStringGreat/1666-MakeTheStringGreat.cpp
@@ -0,0 +1,28 @@
+// Last updated: 3/7/2026, 4:12:01 PM
+class Solution {
+public:
+    string makeGood(string s) {
+        stack<char>stk;
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(!stk.empty() && (stk.top()-'a'==s[i]-'A' || stk.top()-'A'==s[i]-'a'))
+            {
+                stk.pop();
+                continue;
+            }
+            stk.push(s[i]);
+        }
+
+        string res="";
+
+        while(!stk.empty())
+        {
+            res+=stk.top();
+            stk.pop();
+        }
+
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
