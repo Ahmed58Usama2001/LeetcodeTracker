3dda179e8c2a9b3eb60ede28b18967a28a9e47f1
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:12:10 +0200

    Create 1539-DiagonalTraverseIi

---
+++ 1539-DiagonalTraverseIi/1539-DiagonalTraverseIi.cpp
@@ -0,0 +1,24 @@
+// Last updated: 3/7/2026, 4:12:10 PM
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+        unordered_map<int,vector<int>>mp;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            for(int j=0;j<nums[i].size();j++)
+            {
+                int sum = i+j;
+                mp[sum].push_back(nums[i][j]);
+            }
+        }
+
+        vector<int>res;
+        for(int i = 0;i<mp.size();i++)
+            for(auto it = mp[i].rbegin() ; it!=mp[i].rend() ; it++)
+            res.push_back(*it);
+        
+        return res;
+
+    }
+};
