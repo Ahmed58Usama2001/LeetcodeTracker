606be1f3e442692e16ac7814823b73ad79f96a65
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:15:36 +0200

    Create 975-RangeSumOfBst

---
+++ 975-RangeSumOfBst/975-RangeSumOfBst.cpp
@@ -0,0 +1,35 @@
+// Last updated: 3/7/2026, 4:15:35 PM
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+       
+        int sum=0;
+
+        return RangeSum(root,low,high,sum);
+    }
+
+    int RangeSum(TreeNode* root, int low, int high,int& sum) {
+       
+        if(root->val >=low && root->val<=high )
+        sum+= root->val;
+
+        if(root->left)
+        RangeSum(root->left,  low,  high,sum);
+
+        if(root->right)
+        RangeSum(root->right,  low,  high,sum);
+
+        return sum;
+    }
+};
