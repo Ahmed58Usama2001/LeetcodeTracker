cea2f23542e4acb282a482e7dc2da4077c5580ff
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:15:39 +0200

    Create 940-FruitIntoBaskets

---
+++ 940-FruitIntoBaskets/940-FruitIntoBaskets.cpp
@@ -0,0 +1,27 @@
+// Last updated: 3/7/2026, 4:15:38 PM
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int left=0,right=0,res=0;
+        unordered_map<int,int>mp;
+
+        while(right<fruits.size())
+        {
+            mp[fruits[right]]++;
+
+            if(mp.size()>2)
+            {
+                if(--mp[fruits[left]]==0)
+                mp.erase(fruits[left]);
+
+                left++;
+            }
+            else
+            res=max(res,right-left+1);
+
+
+            right++;
+        }
+        return res;
+    }
+};
