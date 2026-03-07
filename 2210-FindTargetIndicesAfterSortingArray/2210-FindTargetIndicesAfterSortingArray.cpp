59ae2e0ce813c2dc1dd215d6a65111973a363a4f
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:11:40 +0200

    Create 2210-FindTargetIndicesAfterSortingArray

---
+++ 2210-FindTargetIndicesAfterSortingArray/2210-FindTargetIndicesAfterSortingArray.cpp
@@ -0,0 +1,15 @@
+// Last updated: 3/7/2026, 4:11:40 PM
+class Solution {
+public:
+    vector<int> targetIndices(vector<int>& nums, int target) {
+        sort(nums.begin() , nums.end());
+        int lower = lower_bound(nums.begin() , nums.end() , target)- nums.begin();
+        int upper = upper_bound(nums.begin() , nums.end() , target)- nums.begin();
+        vector<int>res;
+
+        for(int i=lower;i<upper ; i++)
+        res.push_back(i);
+
+        return res;
+    }
+};
