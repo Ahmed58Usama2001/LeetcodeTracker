4993eaedc2ef859ca61afab17bc45b83fe103fcf
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:12:23 +0200

    Create 1408-FindTheSmallestDivisorGivenAThreshold

---
+++ 1408-FindTheSmallestDivisorGivenAThreshold/1408-FindTheSmallestDivisorGivenAThreshold.cpp
@@ -0,0 +1,25 @@
+// Last updated: 3/7/2026, 4:12:22 PM
+class Solution {
+public:
+
+    bool possible(vector<int>& nums,int num ,int threshold)
+    {
+        long long sum=0;
+        for(int i=0;i<nums.size();i++)
+            sum+=ceil((double)nums[i]/(double)num);
+        return sum<=threshold;
+    }
+
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int start = 1,mid,ans=1, end = *max_element(nums.begin(),nums.end());
+
+        while(start<=end)
+        {   mid = start + (end-start)/2;
+            if(possible(nums , mid , threshold))
+            ans = mid , end=mid-1;
+            else
+            start=mid+1;
+        }
+        return ans;
+    }
+};
