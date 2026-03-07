96cce05b3e88614cdb382250784d6a5d3f4b500c
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:15:16 +0200

    Create 1184-CarPooling

---
+++ 1184-CarPooling/1184-CarPooling.cpp
@@ -0,0 +1,24 @@
+// Last updated: 3/7/2026, 4:15:16 PM
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        vector<pair<int,int>>triggers;
+        int filled =0;
+        for(int i=0;i<trips.size();i++)
+        {
+            triggers.push_back({trips[i][1] , trips[i][0]});
+            triggers.push_back({trips[i][2] , -trips[i][0]});
+        }
+
+        sort(triggers.begin() , triggers.end());
+
+        for(int i=0;i<triggers.size();i++)
+        {
+            filled+=triggers[i].second;
+            if(filled>capacity)
+            return false;
+        }
+
+        return true;
+    }
+};
