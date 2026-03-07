cfa236f978b794393b8edb50d49cdc33a78fdfa3
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:15:58 +0200

    Create 739-DailyTemperatures

---
+++ 739-DailyTemperatures/739-DailyTemperatures.cpp
@@ -0,0 +1,21 @@
+// Last updated: 3/7/2026, 4:15:58 PM
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        vector<int>res(temperatures.size(),0);
+        stack<int>pos;
+
+        for(int i=temperatures.size()-1;i>=0;i--)
+        {
+            while(!pos.empty() && temperatures[pos.top()]<=temperatures[i])
+            pos.pop();
+
+            if(!pos.empty())
+            res[i]=pos.top()-i;
+
+            pos.push(i);
+        }
+
+        return res;
+    }
+};
