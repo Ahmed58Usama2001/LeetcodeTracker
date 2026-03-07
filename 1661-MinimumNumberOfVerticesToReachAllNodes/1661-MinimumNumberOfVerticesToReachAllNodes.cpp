17d8bcf9a931f867328a92d3f654d8fbaa995150
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:12:04 +0200

    Create 1661-MinimumNumberOfVerticesToReachAllNodes

---
+++ 1661-MinimumNumberOfVerticesToReachAllNodes/1661-MinimumNumberOfVerticesToReachAllNodes.cpp
@@ -0,0 +1,22 @@
+// Last updated: 3/7/2026, 4:12:04 PM
+class Solution {
+public:
+
+  
+
+    vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
+
+        vector<bool>has_in(n);
+        vector<int>res;
+
+        for(int i =0; i<edges.size();i++ )
+        has_in[edges[i][1]]=true;
+
+        for(int i=0;i<n;i++)
+        if(!has_in[i])
+        res.push_back(i);
+
+       
+        return res;
+    }
+};
