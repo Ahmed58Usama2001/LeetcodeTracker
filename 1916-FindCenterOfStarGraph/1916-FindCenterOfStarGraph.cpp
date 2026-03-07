7d490df37cfe30d1bf3024f449ff5f5d7fdeda48
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:11:54 +0200

    Create 1916-FindCenterOfStarGraph

---
+++ 1916-FindCenterOfStarGraph/1916-FindCenterOfStarGraph.cpp
@@ -0,0 +1,20 @@
+// Last updated: 3/7/2026, 4:11:54 PM
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        vector<int>count(edges.size()+2);
+
+        for(int i=0;i<edges.size();i++)
+        {
+            count[edges[i][0]]++;
+            count[edges[i][1]]++;
+        }
+
+        for(int i=0;i<count.size();i++)
+         if(count[i]==edges.size())
+         return i;
+       
+
+        return -1;
+    }
+};
