74dc4c861124fa670b783e4849c9608861442975
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:16:09 +0200

    Create 659-SplitArrayIntoConsecutiveSubsequences

---
+++ 659-SplitArrayIntoConsecutiveSubsequences/659-SplitArrayIntoConsecutiveSubsequences.cpp
@@ -0,0 +1,36 @@
+// Last updated: 3/7/2026, 4:16:08 PM
+class Solution {
+public:
+    bool isPossible(vector<int>& nums) {
+        unordered_map<int,int>freq , need;
+
+        for(int n:nums)
+        freq[n]++;
+
+        for(int n:nums)
+        {
+            if(freq[n]==0)
+            continue;
+
+            if(need[n]>0)
+            {
+                need[n]--;
+                freq[n]--;
+
+                need[n+1]++;
+            }
+            else if(freq[n]>0 && freq[n+1]>0 && freq[n+2]>0)
+            {
+                freq[n]--;
+                freq[n+1]--;
+                freq[n+2]--;
+
+                need[n+3]++;
+            }
+            else
+            return false;
+        }
+
+        return true;
+    }
+};
