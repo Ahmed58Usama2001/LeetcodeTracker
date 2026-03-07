d9e5d015f067359e5b728342cedb25f2f1bf042d
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:15:19 +0200

    Create 1137-HeightChecker

---
+++ 1137-HeightChecker/1137-HeightChecker.cpp
@@ -0,0 +1,16 @@
+// Last updated: 3/7/2026, 4:15:19 PM
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int>expected(heights);
+        int res=0;
+
+        sort(expected.begin(),expected.end());
+
+        for(int i=0;i<heights.size();i++)
+        if(heights[i]!=expected[i])
+        res++;
+
+        return res;
+    }
+};
