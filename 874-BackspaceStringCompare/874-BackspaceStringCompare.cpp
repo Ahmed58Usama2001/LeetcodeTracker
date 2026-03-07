4da657db7d837574d655ec5cb15e4da6b2ff8dc4
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:15:43 +0200

    Create 874-BackspaceStringCompare

---
+++ 874-BackspaceStringCompare/874-BackspaceStringCompare.cpp
@@ -0,0 +1,41 @@
+// Last updated: 3/7/2026, 4:15:43 PM
+class Solution {
+public:
+    bool backspaceCompare(string s, string t) {
+        stack<char> stk1;
+        stack<char>stk2;
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(isalpha(s[i]))
+            stk1.push(s[i]);
+             else if(s[i]=='#' && !stk1.empty())
+            stk1.pop();
+        }
+
+        for(int i=0;i<t.length();i++)
+        {
+            if(isalpha(t[i]))
+            stk2.push(t[i]);
+            else if(t[i]=='#' && !stk2.empty())
+            stk2.pop();
+        }
+
+        string s2="";
+        string t2="";
+
+        while(!stk1.empty())
+        {
+            s2+=stk1.top();
+            stk1.pop();
+        }
+
+        while(!stk2.empty())
+        {
+            t2+=stk2.top();
+            stk2.pop();
+        }
+
+        return s2==t2;
+    }
+};
