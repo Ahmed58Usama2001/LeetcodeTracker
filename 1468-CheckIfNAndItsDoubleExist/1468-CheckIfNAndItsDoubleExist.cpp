62d8996216b0e80b3c3bd6d1469f0972fac237c5
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:12:16 +0200

    Create 1468-CheckIfNAndItsDoubleExist

---
+++ 1468-CheckIfNAndItsDoubleExist/1468-CheckIfNAndItsDoubleExist.cpp
@@ -0,0 +1,29 @@
+// Last updated: 3/7/2026, 4:12:15 PM
+class Solution {
+public:
+    bool checkIfExist(vector<int>& arr) {
+        sort(arr.begin() , arr.end());
+        int n = arr.size();
+        
+        for(int i=0;i<n;i++)
+        {
+            int start=0,end=n-1,mid;
+            while(start<=end)
+            {
+                mid=start+(end-start)/2;
+
+                if(arr[i]*2==arr[mid] && i!=mid)
+                return true;
+
+                else if(arr[mid]>arr[i]*2)
+                end=mid-1;
+
+                else
+                start=mid+1;
+            }
+            
+        }
+
+        return false;
+    }
+};
