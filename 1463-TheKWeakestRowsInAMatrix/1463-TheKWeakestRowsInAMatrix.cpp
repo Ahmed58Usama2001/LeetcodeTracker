ee1e7d3b6726b77b75a9950011b935b6da70a6ae
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:12:18 +0200

    Create 1463-TheKWeakestRowsInAMatrix

---
+++ 1463-TheKWeakestRowsInAMatrix/1463-TheKWeakestRowsInAMatrix.cpp
@@ -0,0 +1,28 @@
+// Last updated: 3/7/2026, 4:12:17 PM
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+        int counter=0;
+        for(int i=0;i<mat.size();i++)
+        {   counter=0;
+            for(int j=0;j<mat[i].size();j++)
+            {
+                if(mat[i][j])
+                counter++;
+                else
+                break;
+            }
+            pq.push({counter , i});
+        }
+
+        vector<int>res;
+        while(k--)
+        {
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+        return res;
+    }
+};
