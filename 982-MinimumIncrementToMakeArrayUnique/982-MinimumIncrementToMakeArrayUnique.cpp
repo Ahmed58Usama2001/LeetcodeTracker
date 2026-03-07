7c5945ea356f16e9a89a8567f5a9d3040dd96ccf
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:15:35 +0200

    Create 982-MinimumIncrementToMakeArrayUnique

---
+++ 982-MinimumIncrementToMakeArrayUnique/982-MinimumIncrementToMakeArrayUnique.cpp
@@ -0,0 +1,21 @@
+// Last updated: 3/7/2026, 4:15:34 PM
+class Solution {
+public:
+    int minIncrementForUnique(vector<int>& nums) {
+        int ans=0;
+        sort(nums.begin(),nums.end());
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]==nums[i-1])
+            {nums[i]++;
+            ans++;
+            }
+            else if(nums[i]<nums[i-1])
+            {
+                ans+=(nums[i-1]+1-nums[i]);
+                nums[i]=nums[i-1]+1;
+            }   
+        }
+return ans;
+    }
+};
