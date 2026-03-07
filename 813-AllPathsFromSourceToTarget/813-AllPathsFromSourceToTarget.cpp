49b33b24426db84093fe0f2e01925020d80180ee
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:15:46 +0200

    Create 813-AllPathsFromSourceToTarget

---
+++ 813-AllPathsFromSourceToTarget/813-AllPathsFromSourceToTarget.cpp
@@ -0,0 +1,29 @@
+// Last updated: 3/7/2026, 4:15:46 PM
+class Solution {
+public:
+
+    void dfs(vector<vector<int>>& graph,vector<vector<int>>& result ,vector<int>&res
+    ,int node,int dest )
+    {
+        res.push_back(node);
+        if(node==dest)
+        {
+            result.push_back(res);
+            res.pop_back();
+            return;
+        }
+        for(auto neighbor : graph[node])
+        {
+            dfs(graph ,result ,res ,  neighbor,dest);
+        }
+        res.pop_back();
+    }
+
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> result;
+        vector<int>res;
+        dfs(graph,result ,res,0 , graph.size()-1);
+
+        return result;
+    }
+};
