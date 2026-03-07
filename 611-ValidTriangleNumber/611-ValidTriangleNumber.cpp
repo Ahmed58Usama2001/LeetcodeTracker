cee0d3ba614831f531ec46187b78331258629251
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:16:16 +0200

    Create 611-ValidTriangleNumber

---
+++ 611-ValidTriangleNumber/611-ValidTriangleNumber.cpp
@@ -0,0 +1,33 @@
+// Last updated: 3/7/2026, 4:16:15 PM
+class Solution {
+public:
+
+    int BS_First_valid(vector<int>& nums , int start , int val)
+    {
+        int end = nums.size()-1,mid,position=nums.size();
+
+        while(start<=end)
+        {
+            mid = start + (end-start)/2;
+            if(nums[mid]<val)
+            start=mid+1;
+            else
+            position = mid , end=mid-1;
+        }
+        return position;
+    }
+
+    int triangleNumber(vector<int>& nums) {
+        int n = nums.size(),count=0,k;
+        sort(nums.begin(),nums.end());
+        for (int i=0;i<n-2;i++)
+        {
+            k=i+2;
+            for(int j=i+1 ; j<n-1 && nums[i]!=0; j++)
+                count+=BS_First_valid(nums , k , nums[i]+nums[j])-j-1;
+            
+        }
+
+        return count;
+    }
+};
