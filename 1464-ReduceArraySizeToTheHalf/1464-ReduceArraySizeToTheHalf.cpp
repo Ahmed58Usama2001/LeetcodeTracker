e12bee864c0f591e49e40ad7ff0a0b4fbee78848
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:12:17 +0200

    Create 1464-ReduceArraySizeToTheHalf

---
+++ 1464-ReduceArraySizeToTheHalf/1464-ReduceArraySizeToTheHalf.cpp
@@ -0,0 +1,28 @@
+// Last updated: 3/7/2026, 4:12:16 PM
+class Solution {
+public:
+    int minSetSize(vector<int>& arr) {
+
+        int n = arr.size(), target = n/2;
+
+        unordered_map<int,int>mp;
+        priority_queue<int>pq;
+
+        for(int i =0;i<n;i++)
+        mp[arr[i]]++;
+
+        for (auto& pair : mp) 
+        pq.push(pair.second);
+
+        int counter=0;
+        while(n>target)
+        {
+            n-=pq.top();
+            pq.pop();
+            counter++;
+        }
+
+        return counter;
+    }
+    
+};
