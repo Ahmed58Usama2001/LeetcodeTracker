6e0ef280397d75b3a3137def9c9696e3e6602c91
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:16:22 +0200

    Create 567-PermutationInString

---
+++ 567-PermutationInString/567-PermutationInString.cpp
@@ -0,0 +1,27 @@
+// Last updated: 3/7/2026, 4:16:22 PM
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n=s1.length(),start=0;
+
+        if(n>s2.length())
+        return false;
+
+        sort(s1.begin(), s1.end());
+
+        string substring;
+
+        while(start<s2.length()-n+1)
+        {
+            substring=s2.substr(start, n);
+            sort(substring.begin(), substring.end());
+
+            if(s1==substring)
+            return true;
+            else
+            start++;
+        }
+
+        return false;
+    }
+};
