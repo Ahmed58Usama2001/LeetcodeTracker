efebca51b4a73c42834e887951ce20d14699ff67
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:11:53 +0200

    Create 1966-FrequencyOfTheMostFrequentElement

---
+++ 1966-FrequencyOfTheMostFrequentElement/1966-FrequencyOfTheMostFrequentElement.cpp
@@ -0,0 +1,18 @@
+// Last updated: 3/7/2026, 4:11:52 PM
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k) {
+    sort(nums.begin(),nums.end());
+    long long left=0,right=0,ans=0, sum=0;
+
+    while(right<nums.size())
+    {   sum+=nums[right];
+        while( nums[right] * (right-left+1) > sum+k)
+            sum-=nums[left++];
+        ans=max(ans,right-left+1);
+        right++;
+    }
+
+    return ans;
+    }
+};
