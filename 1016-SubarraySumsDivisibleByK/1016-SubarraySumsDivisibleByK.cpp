d58eaf54e6b42036e2309aef887a6a3820d9ca80
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:15:27 +0200

    Create 1016-SubarraySumsDivisibleByK

---
+++ 1016-SubarraySumsDivisibleByK/1016-SubarraySumsDivisibleByK.cpp
@@ -0,0 +1,18 @@
+// Last updated: 3/7/2026, 4:15:27 PM
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+        int prefixMod=0,res=0;
+        vector<int> modGroups(k);
+        modGroups[0]=1;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            prefixMod=(prefixMod + nums[i] % k + k) % k;
+            res+=modGroups[prefixMod];
+            modGroups[prefixMod]++;
+        }
+
+        return res;
+    }
+};
