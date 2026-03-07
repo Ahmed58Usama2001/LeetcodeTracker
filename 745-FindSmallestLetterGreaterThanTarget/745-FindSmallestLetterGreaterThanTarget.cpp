1ac332e61a3acce8dceeccdada042428d5035bfb
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:15:57 +0200

    Create 745-FindSmallestLetterGreaterThanTarget

---
+++ 745-FindSmallestLetterGreaterThanTarget/745-FindSmallestLetterGreaterThanTarget.cpp
@@ -0,0 +1,21 @@
+// Last updated: 3/7/2026, 4:15:57 PM
+class Solution {
+public:
+    char nextGreatestLetter(vector<char>& letters, char target) {
+        int left = 0 , right = letters.size(),mid,ans;
+        while(left<right)
+        {
+            mid =left+ (right-left)/2;
+
+            if(letters[mid]>target)
+            {
+            right=mid;
+            ans=mid;
+            }
+            else
+            left=mid+1;
+        }
+
+        return letters[ans];
+    }
+};
