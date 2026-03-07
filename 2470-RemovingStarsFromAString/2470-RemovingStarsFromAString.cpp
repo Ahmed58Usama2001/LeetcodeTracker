91e4d6faa10f208f4f37e4248f173c3641b889e9
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-07 16:11:34 +0200

    Create 2470-RemovingStarsFromAString

---
+++ 2470-RemovingStarsFromAString/2470-RemovingStarsFromAString.cpp
@@ -0,0 +1,26 @@
+// Last updated: 3/7/2026, 4:11:34 PM
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char>stk;
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(s[i]!='*')
+            stk.push(s[i]);
+            else 
+                stk.pop();
+        }
+
+        string res="";
+        while(!stk.empty())
+        {
+            res+=stk.top();
+            stk.pop();
+        }
+
+        reverse(res.begin(),res.end());
+
+        return res;
+    }
+};
