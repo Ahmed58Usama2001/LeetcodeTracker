2e464f49432dfcc74457ff9f63c1921a670f6fc1
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-03-08 11:55:12 +0200

    Create 506-RelativeRanks

---
+++ 506-RelativeRanks/506-RelativeRanks.cpp
@@ -0,0 +1,32 @@
+// Last updated: 3/8/2026, 11:55:12 AM
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+        priority_queue<pair<int,int>>pq;
+        vector<string>ans(score.size());
+
+        for(int i=0;i<score.size();i++){
+            pq.push({score[i],i});
+        }
+
+        vector<string>ranks = {"Gold Medal","Silver Medal","Bronze Medal"};
+        int idx=1;
+        while(!pq.empty()){
+            auto top=pq.top();
+            pq.pop();
+            
+            int index=top.second;
+
+            if(idx<4){
+                ans[index]=ranks[idx-1];
+            }else{
+                ans[index]=to_string(idx);
+            }
+            idx++;
+        }
+        return ans;
+    }
+    
+
+
+};
