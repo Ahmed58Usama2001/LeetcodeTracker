2d3408271e2ff4bf34c2775635ea54db8adf8468
Ahmed Usama <109095007+Ahmed58Usama2001@users.noreply.github.com>
2026-04-26 13:22:04 +0300

    Create 219-ContainsDuplicateII

---
+++ 219-ContainsDuplicateII/version/csharp/219-ContainsDuplicateII_20260426_132204.cs
@@ -0,0 +1,19 @@
+// Last updated: 4/26/2026, 1:22:04 PM
+1public class Solution {
+2    public bool ContainsNearbyDuplicate(int[] nums, int k) {
+3        Dictionary<int,int> dict = new Dictionary<int,int>();
+4
+5        for(int i=0;i<nums.Length;i++){
+6            if(!dict.ContainsKey(nums[i]))
+7            dict.Add(nums[i],i);
+8            else{
+9                if(Math.Abs(i-dict[nums[i]])<=k)
+10                return true;
+11                else
+12                dict[nums[i]]=i;
+13            }
+14        }
+15
+16        return false;
+17    }
+18}
